2f8ae102e42c879f57a424fd410f1962996bc6e8
Halloloid <animation12321567@gmail.com>
2026-07-29 23:31:16 +0530

    Added Audit logs for permissions

--- src/services/permission.rs
+++ src/services/permission.rs
@@ -2,14 +2,10 @@ use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::{
-    config::response_config::AppError,
-    models::permission::Permission,
-    repositories::{
-        organization::check_permission,
-        permission::{
+    config::response_config::AppError, models::permission::Permission, repositories::{
+        audit_logs::write_audit_logs, organization::check_permission, permission::{
             all_permissions, assign_permission, delete_permission_of_role, role_permission,
-        },
-        role::paticular_role,
+        }, role::paticular_role,
     },
 };
 
@@ -40,7 +36,9 @@ pub async fn assign_permissions_service(
         return Err(AppError::NotFound);
     }
 
-    assign_permission(pool, permission_ids, role_id).await?;
+    assign_permission(pool, permission_ids.clone(), role_id).await?;
+
+    let _ = write_audit_logs(pool, "permission:assigned", user_id, &format!("organization:{}/role:{}/permission:{:#?}",org_id,role_id,permission_ids)).await;
 
     Ok(())
 }
@@ -80,7 +78,8 @@ pub async fn delete_permission_of_role_service(
         return Err(AppError::NotFound);
     }
 
-    delete_permission_of_role(pool, permission_ids, role_id).await?;
+    delete_permission_of_role(pool, permission_ids.clone(), role_id).await?;
 
+    let _ = write_audit_logs(pool, "permission:removed", user_id, &format!("organization:{}/role:{}/permission:{:#?}",org_id,role_id,permission_ids)).await;
     Ok(())
 }
