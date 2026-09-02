f0d3a92471174bafbd9308a584b9142db2d033b9
Halloloid <animation12321567@gmail.com>
2026-09-03 00:10:27 +0530

    Added Helper Function to Distinguish Normal User Auth with api auth

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,3 +1,11 @@
+use sqlx::PgPool;
+use uuid::Uuid;
+
+use crate::{
+    config::{auth_config::AuthContext, response_config::AppError},
+    repositories::organization::check_permission,
+};
+
 pub mod api_key;
 pub mod audit_logs;
 pub mod health;
@@ -7,3 +15,32 @@ pub mod permission;
 pub mod role;
 pub mod session;
 pub mod user;
+
+pub async fn resolver_user_id(
+    auth: &AuthContext,
+    required_permission: &str,
+    org_id: Uuid,
+    pool: PgPool,
+) -> Result<Option<Uuid>, AppError> {
+    match auth {
+        AuthContext::User(claims) => {
+            if !check_permission(&pool, claims.sub, org_id, required_permission).await? {
+                return Err(AppError::Forbidden);
+            }
+
+            Ok(Some(claims.sub))
+           
+        }
+        AuthContext::ApiKey(api_key_record) => {
+
+            if api_key_record.org_id != org_id{
+                return Err(AppError::Forbidden);
+            }
+
+            if !api_key_record.scopes.contains(&required_permission.to_string()){
+                return Err(AppError::Forbidden);
+            }
+            Ok(None)
+        },
+    }
+}
